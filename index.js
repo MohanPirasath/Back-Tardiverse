df2cb0719b4113ed18219f1da5d5d6de8b292c5e
Mohanpirasath <mahendran.mohanmass2000@gmail.com>
2022-10-18 22:30:28 +0530

    api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://flightbooking-tardiverse-app.netlify.app"],
     methods:["GET","POST","PUT","DELETE"],
 }
 ))
